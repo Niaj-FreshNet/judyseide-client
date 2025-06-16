da8ed7baf45a5c208ec061d2d2e8c801bf6fc932
Niaj-FreshNet <pensiveniaj@gmail.com>
2025-06-17 01:34:00 +0600

    hello

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 // next.config.js
 
 module.exports = {
+  // Disable ESLint during builds (optional, only use if you're confident)
   eslint: {
     ignoreDuringBuilds: true,
   },
+  
+  // Configure remote image sources for DigitalOcean Spaces
   images: {
     remotePatterns: [
       {
@@ -12,5 +15,10 @@ module.exports = {
         pathname: '**',
       },
     ],
+  },
+  
+  // Enable Webpack 5 for better performance (optional, if not already enabled)
+  future: {
+    webpack5: true,
   },
 };
